b2c9c95dce943d12a8da2a20f3539c689858c80b
= <=>
2022-10-10 15:56:37 -0400

    Commit to main -Max

---
+++ Player.h
@@ -0,0 +1,35 @@
+// #pragma once
+
+// #include "Cards.h"
+// #include "Map.h"
+// #include "Orders.h"
+class Map;
+#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+class Player
+{
+    public:
+        Player(); //default constructor
+        ~Player(); //destructor
+        // Player(string playerName, vector<Map::Territory*> territoriesOwned, vector<Cards*> playerHand, vector<Orders*> ordersIssued);
+        Player(const Player&); //copy constructor
+        
+
+        //Data members
+        string* name;
+        vector<Map::Territory> *territories;
+        // vector<Cards*> *hand;
+        // vector<Orders*> *orders;
+
+        void toDefend(); 
+        void toAttack();
+        void issueOrder();
+
+        //vector<Map::Territory*> *defenceList;
+        //vector<Map::Territory*> *attackList;
+};
+
+string testPlayers();
